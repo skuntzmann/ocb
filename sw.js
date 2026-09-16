a0c0589f4157de73efa9f695e3fc3af6c5f191d1
Sylvain Kuntzmann <13257444+skuntzmann@users.noreply.github.com>
2026-09-16 20:54:27 +0200

    Change cache name to v3 and enhance comments
    Updated cache name to v3 and improved comments.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ```javascript
-const CACHE_NAME = "ocb-v2";
+const CACHE_NAME = "ocb-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,42 +9,62 @@ const FILES_TO_CACHE = [
   "./icons/icon-512.png"
 ];
 
-// Installation
+/*
+ * Installation du service worker
+ */
 self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
       .then(() => self.skipWaiting())
+
   );
+
 });
 
-// Activation
+
+/*
+ * Activation
+ */
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+
+    caches.keys()
+      .then(keys => {
+
+        return Promise.all(
+
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+
+        );
+
+      })
+      .then(() => self.clients.claim())
+
   );
+
 });
 
-// Gestion des requêtes
+
+/*
+ * Requêtes
+ *
+ * On laisse le réseau fonctionner normalement.
+ * Le cache sert uniquement de secours.
+ */
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-
-        return response;
-      })
       .catch(() => caches.match(event.request))
+
   );
+
 });
 ```
